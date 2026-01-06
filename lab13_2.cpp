8e333ae175df7c0241226dd49055521cc1682aef
Traipop <traipopwichiansarn@gmail.com>
2026-01-06 17:55:12 +0700

    lab13_2 completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,29 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool img[N][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for(int i = 0; i < N ; i++){
+        cout << "|";
+        for (int j = 0 ; j < M ; j++){
+            if(img[i][j]){
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << "|\n";
+    }
+    cout << "------------------------------------------------------------------------\n";
+}
+
+void updateImage(bool img[N][M],int s,int x,int y){
+    for(int i = 0; i < N ; i++){
+        for (int j = 0 ; j < M ; j++){
+            if ((sqrt((pow((i-x),2))+(pow((j-y),2)))) <= s-1){
+                img[i][j] = 1;
+            }
+        }
+    }
+}
