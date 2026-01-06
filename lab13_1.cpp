39348ae0476d479057c4042f625b612471ee7a42
Traipop <traipopwichiansarn@gmail.com>
2026-01-06 15:39:21 +0700

    lab13_1 completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,22 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+ void stat(const double A[],int N,double B[]){
+    double sum = 0,sum_sq = 0,sum_GM = 1,sum_H = 0,max_val = 0, min_val=13; 
+    for (int i = 0; i < N; i++)
+    {
+        sum += A[i];
+        sum_sq += pow(A[i],2);
+        sum_GM *= A[i];
+        sum_H += 1/A[i];
+        if (A[i] > max_val) max_val = A[i];
+        if (A[i] < min_val) min_val = A[i];
+    }
+
+    B[0] = sum/N;
+    B[1] = sqrt((sum_sq/N)-pow(B[0],2));
+    B[2] = pow(sum_GM,(1.0/N));
+    B[3] = N/sum_H;
+    B[4] = max_val;
+    B[5] = min_val;
+ }
